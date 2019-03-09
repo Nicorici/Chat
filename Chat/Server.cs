1c955da40d5f89af8795e9522576a938009743db
swcc cwcw <nickorici@yahoo.com>
2019-03-09 20:47:31 +0200

    Add project files.

---
+++ Chat/Server.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Chat
+{
+    public class Server
+    {
+        private static TcpClient client;
+        private TcpListener tcpListener = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5500));
+        private static List<TcpClient> clients = new List<TcpClient>();
+        private NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+
+            var endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5500);
+            var server = new TcpListener(endpoint);
+            NetworkStream stream;
+            byte[] message = new byte[10];
+
+            server.Start();
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                clients.Add(client);
+                stream = client.GetStream();
+                Read(stream, message, "");
+            }
+
+            ////var server = new Server();
+            ////server.Start();
+            ////server.AcceptClients();
+            ////Console.Read();
+        }
+
+        public void Start()
+        {
+            tcpListener.Start();
+        }
+
+
+
+        private void SendReceiveData(Client client)
+        {
+            byte[] buffer = new byte[100];
+            stream = client.TcpClient.GetStream();
+            string name = client.Name;
+            Read(stream, buffer, "");
+        }
+
+        private static void Read(NetworkStream stream, byte[] buffer, string message)
+        {
+            int read = 0;
+            stream.BeginRead(buffer, 0, buffer.Length, r =>
+            {
+                try
+                {
+                    read = stream.EndRead(r);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($" has disconected");
+                    clients.Remove(client);
+                    stream.Dispose();
+                    stream.Close();
+                    return;
+                }
+                string temp = Encoding.ASCII.GetString(buffer);
+                for (int i = 0; i < temp.Length; i++)
+                    if (temp[i] != '\0')
+                        message += temp[i];
+                    else
+                    {
+                        if (message.Length > 0)
+                        {
+                            Console.WriteLine(message);
+                            message += '\0';
+                            buffer = Encoding.ASCII.GetBytes(message);
+                            SendDataToAll(buffer);
+                            message = "";
+                            break; 
+                        }
+                    }
+                Read(stream, buffer, message);
+            }, null);
+
+        }
+
+        private static void SendDataToAll(byte[] message)
+        {
+            NetworkStream stream;
+            foreach (var client in clients)
+            {
+                if (client.Connected)
+                {
+                    stream = client.GetStream();
+                    stream.Write(message);
+                }
+                else
+                {
+                    clients.Remove(client);
+                }
+            }
+        }
+    }
+}
---
+++ Client/Client.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Chat
+{
+    public class Client
+    {
+        public string Name { get; }
+        public TcpClient TcpClient { get; set; }
+        public bool IsConnected { get => TcpClient.Connected; }
+
+        public Client()
+        {
+            Console.Write("Please input your name : ");
+            Name = Console.ReadLine();
+            TcpClient = new TcpClient();
+        }
+
+        public void Connect(IPEndPoint endpoint)
+        {
+            try
+            {
+                TcpClient.Connect(endpoint);
+                SendReceiveData();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+
+        public void Close()
+        {
+            TcpClient.Dispose();
+            TcpClient.Close();
+        }
+
+        private void SendReceiveData()
+        {
+            if (IsConnected)
+            {
+                var stream = TcpClient.GetStream();
+                byte[] buffer = Encoding.ASCII.GetBytes($"{Name} has connected."+'\0');
+                stream.Write(buffer);
+
+                ReadStream(stream, buffer, "");
+                WriteStream(stream, buffer);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new Client();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5500));
+            Console.Read();
+        }
+
+        private  void WriteStream(NetworkStream stream, byte[] buffer)
+        {
+            buffer = Encoding.ASCII.GetBytes( Name + Console.ReadLine()+'\0');
+            stream.BeginWrite(buffer, 0, buffer.Length, r =>
+            {
+                stream.EndWrite(r);
+                WriteStream(stream, buffer);
+            }, null);
+
+        }
+
+        private static void ReadStream(NetworkStream stream, byte[] buffer, string message)
+        {
+            stream.BeginRead(buffer,
+                0,
+                buffer.Length, r =>
+                {
+                    var read = stream.EndRead(r);
+                    if (read == 0)
+                    {
+                        Console.WriteLine("Server has disconnected.");
+                        return;
+                    }
+                    string temp = Encoding.ASCII.GetString(buffer);
+                    for (int i = 0; i < temp.Length; i++)
+                        if (temp[i] != '\0')
+                            message += temp[i];
+                        else
+                        {
+                            if (message.Length > 0)
+                            {
+                                Console.WriteLine(message);
+                                message = "";
+                                break;
+                            }
+                        }
+                    ReadStream(stream, buffer, message);
+                },
+                null);
+        }
+    }
+}
